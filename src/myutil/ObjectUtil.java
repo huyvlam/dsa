d8b5380bc379c25fdb6023c03564d4e809f10a99
Huy (Randy) <huyvlam@gmail.com>
2026-03-22 15:35:48 -0700

    rename Helper to ObjectUtil

--- src/myutil/ObjectUtil.java
+++ src/myutil/ObjectUtil.java
@@ -1,19 +1,8 @@
-package myhelper;
+package myutil;
 
-import mymodel.Person;
-
-import java.util.Comparator;
-import java.util.Objects;
-import java.util.Random;
-
-public class Helper {
+public class ObjectUtil {
     // Return true if the given object is of the same type as target element
     public static <E> boolean isCompatible(Object obj, E target) {
         return target.getClass().isInstance(obj);
-    }
-
-    static void main(String args[]) {
-        Object obj = new Object();
-        IO.println(isCompatible(new Object(), new Person("Zizi", 21)));
     }
 }
