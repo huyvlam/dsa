7982d126a745ea38762eebd52d561de4217bd7b0
Huy (Randy) <huyvlam@gmail.com>
2026-03-23 09:45:26 -0700

    Test coverage for addFirst, addLast, peekFirst, peekLast

---
+++ test/mylinked/singly/CircularLinkedListTest.java
@@ -0,0 +1,28 @@
+package mylinked.singly;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircularLinkedListTest {
+    private CircularLinkedList<String> list;
+
+    @BeforeEach
+    void setUp() {
+        list = new CircularLinkedList<>(null);
+    }
+
+    @Test
+    @DisplayName("Should add at both ends and maintain head/tail")
+    void testAddFirstLast() {
+        list.addLast("Kiwi");
+        list.addFirst("Mangosteen");
+        list.addLast("Passionfruit");
+
+        assertEquals("Mangosteen", list.peekFirst());
+        assertEquals("Passionfruit", list.peekLast());
+        assertEquals(3, list.size());
+    }
+}
