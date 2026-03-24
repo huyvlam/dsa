a8f68f5aa2ccad30e8692ae4841fad2651b9fdb7
Huy (Randy) <huyvlam@gmail.com>
2026-03-23 17:26:30 -0700

    Create base design for CircularDoublyLinkedList

---
+++ src/mylinked/doubly/CircularDoublyLinkedList.java
@@ -0,0 +1,32 @@
+package mylinked.doubly;
+
+import java.util.Comparator;
+
+public class CircularDoublyLinkedList<E> {
+    private DoublyNode<E> sentinel;
+    private int count;
+    private final Comparator<? super E> comparator;
+
+    public CircularDoublyLinkedList(Comparator<? super E> comparator) {
+        sentinel = new DoublyNode<>(null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        count = 0;
+        this.comparator = (comparator != null) ? comparator : (a, b) -> {
+            if (a instanceof Comparable && b instanceof Comparable)
+                return ((Comparable) a).compareTo(b);
+
+            return a.equals(b) ? 0 : -1;
+        };
+    }
+
+    public void clear() {
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        count = 0;
+    }
+
+    public int size() {
+        return count;
+    }
+}
