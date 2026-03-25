d0d8b4d7bf4042fbfa213e35b866df4438b5a458
Huy (Randy) <huyvlam@gmail.com>
2026-03-25 09:10:17 -0700

    Rename methods to checkNullArgument, checkBound

--- src/myhelper/Checker.java
+++ src/myhelper/Checker.java
@@ -1,16 +1,16 @@
 package myhelper;
 
 public class Checker {
-    public static <E> void checkNotNull(E data) {
-        checkNotNull(data, "Data");
+    public static <E> void checkNullArgument(E data) {
+        checkNullArgument(data, "Data");
     }
 
-    public static <E> void checkNotNull(E data, String name) {
+    public static <E> void checkNullArgument(E data, String name) {
         if (data == null)
             throw new IllegalArgumentException(name + " cannot be null");
     }
 
-    public static void checkIndex(int i, int n) {
+    public static void checkBound(int i, int n) {
         if (i < 0 || i >= n)
             throw new IndexOutOfBoundsException("Index cannot be out of bound");
     }
