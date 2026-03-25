8273703107242f63bec5745e8e32bd73cbfb1cac
Huy (Randy) <huyvlam@gmail.com>
2026-03-25 16:20:27 -0700

    Update test coverage for add and remove by index

--- test/mylinked/singly/MyCircularLinkedListTest.java
+++ test/mylinked/singly/MyCircularLinkedListTest.java
@@ -1,5 +1,7 @@
 package mylinked.singly;
 
+import mymodel.Person;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,113 +9,124 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyCircularLinkedListTest {
-    private MyCircularLinkedList<String> list;
+    private MyCircularLinkedList<String> listS;
+    private MyCircularLinkedList<Person> listP;
 
     @BeforeEach
     void setUp() {
-        list = new MyCircularLinkedList<>();
+        listS = new MyCircularLinkedList<>();
+        listP = new MyCircularLinkedList<>();
     }
 
     @Test
     @DisplayName("Should add at both ends and maintain head/tail")
     void testAddFirstLast() {
-        list.addLast("Kiwi");
-        list.addFirst("Mangosteen");
-        list.addLast("Passionfruit");
+        listS.addLast("Kiwi");
+        listS.addFirst("Mangosteen");
+        listS.addLast("Passionfruit");
+        listS.addFirst("Lime");
 
-        assertEquals("Mangosteen", list.peekFirst());
-        assertEquals("Passionfruit", list.peekLast());
-        assertEquals(3, list.size());
+        assertEquals("Lime", listS.peekFirst());
+        assertEquals("Passionfruit", listS.peekLast());
+        assertEquals(4, listS.size());
     }
 
     @Test
     @DisplayName("Should handle pollFirst, pollLast on single element")
     void testPollSingleElement() {
-        list.addFirst("Apple");
-        list.addLast("Cherry");
+        listS.addLast("Cherry");
+        listS.addFirst("Apple");
+        listS.addLast("Berry");
 
-        assertEquals("Apple", list.pollFirst());
-        assertEquals("Cherry", list.pollLast());
-        assertEquals(0, list.size());
+        assertEquals("Apple", listS.pollFirst());
+        assertEquals("Berry", listS.pollLast());
+        assertEquals("Cherry", listS.pollLast());
+        assertEquals(0, listS.size());
     }
 
     @Test
     @DisplayName("Should add/remove element by the given index")
     void testAddRemoveByIndex() {
-        list.addFirst("Peach");
-        list.addLast("Apricot");
-        list.add(1, "Nectarine");
+        listS.add(0, "Nectarine");
+        listS.add(1, "Peach");
+        listS.add(1, "Apricot");
+        listS.add(1, "Plum");
 
-        assertEquals(1, list.indexOf("Nectarine"));
+        assertEquals("Nectarine", listS.peekFirst());
+        assertEquals("Plum", listS.get(1));
+        assertEquals("Apricot", listS.get(2));
+        assertEquals("Peach", listS.peekLast());
 
-        assertEquals("Apricot", list.remove(2));
-        assertEquals("Peach", list.remove(0));
-        assertEquals(1, list.size());
+        assertEquals("Apricot", listS.remove(2));
+        assertEquals("Plum", listS.remove(1));
+        assertEquals("Peach", listS.remove(1));
+        assertEquals("Nectarine", listS.remove(0));
+        assertEquals(0, listS.size());
     }
 
-    @Test
-    @DisplayName("Should remove by the given data")
-    void testRemoveByData() {
-        list.addFirst("Lychee");
-        list.addLast("Longan");
-        list.add(1, "Rambutan");
-
-        assertTrue(list.remove("Rambutan"));
-        assertTrue(list.remove("Lychee"));
-        assertTrue(list.remove("Longan"));
-        assertFalse(list.remove("Apple"));
-        assertEquals(0, list.size());
-    }
-
-    @Test
-    @DisplayName("Should get/replace data by the specified index")
-    void testSetGetByIndex() {
-        list.addFirst("Strawberry");
-        list.addLast("Pluot");
-        list.add(1, "Plum");
-
-        assertEquals("Strawberry", list.get(0));
-        assertEquals("Plum", list.get(1));
-        assertEquals("Pluot", list.get(2));
-
-        assertEquals("Pluot", list.set(2, "Pear"));
-        assertEquals("Pear", list.get(2));
-    }
-
-    @Test
-    @DisplayName("Should throw exception for out of bounds access and invalid argument")
-    void testExceptions() {
-        list.addFirst("Dragon fruit");
-
-        assertThrows(IndexOutOfBoundsException.class, () -> list.get(5));
-        assertThrows(IndexOutOfBoundsException.class, () -> list.set(-1, null));
-        assertThrows(IllegalArgumentException.class, () -> list.indexOf("Lime", null));
-    }
-
-    @Test
-    @DisplayName("Should reverse the list and swap head/tail pointers")
-    void testReverse() {
-        list.addFirst("Tangerine");
-        list.addFirst("Orange");
-        list.addFirst("Kiwi");
-        list.addFirst("Mango");
-
-        list.reverse();
-
-        assertEquals("Tangerine", list.peekFirst());
-        assertEquals("Orange", list.get(1));
-        assertEquals("Kiwi", list.get(2));
-        assertEquals("Mango", list.peekLast());
-    }
-
-    @Test
-    @DisplayName("Should use custom/default printer in toString")
-    void testToString() {
-        list.addFirst("Soursop");
-        String standard = list.toString();
-        String custom = list.toString(p -> ("I like " + p));
-
-        assertTrue(standard.contains("[Soursop] -> (head)"));
-        assertTrue(custom.contains("I like Soursop"));
-    }
+//    @Test
+//    @DisplayName("Should remove by the given data")
+//    void testRemoveByData() {
+//        list.addFirst("Lychee");
+//        list.addLast("Longan");
+//        list.add(1, "Rambutan");
+//
+//        assertTrue(list.remove("Rambutan"));
+//        assertTrue(list.remove("Lychee"));
+//        assertTrue(list.remove("Longan"));
+//        assertFalse(list.remove("Apple"));
+//        assertEquals(0, list.size());
+//    }
+//
+//    @Test
+//    @DisplayName("Should get/replace data by the specified index")
+//    void testSetGetByIndex() {
+//        list.addFirst("Strawberry");
+//        list.addLast("Pluot");
+//        list.add(1, "Plum");
+//
+//        assertEquals("Strawberry", list.get(0));
+//        assertEquals("Plum", list.get(1));
+//        assertEquals("Pluot", list.get(2));
+//
+//        assertEquals("Pluot", list.set(2, "Pear"));
+//        assertEquals("Pear", list.get(2));
+//    }
+//
+//    @Test
+//    @DisplayName("Should throw exception for out of bounds access and invalid argument")
+//    void testExceptions() {
+//        list.addFirst("Dragon fruit");
+//
+//        assertThrows(IndexOutOfBoundsException.class, () -> list.get(5));
+//        assertThrows(IndexOutOfBoundsException.class, () -> list.set(-1, null));
+//        assertThrows(IllegalArgumentException.class, () -> list.indexOf("Lime", null));
+//    }
+//
+//    @Test
+//    @DisplayName("Should reverse the list and swap head/tail pointers")
+//    void testReverse() {
+//        list.addFirst("Tangerine");
+//        list.addFirst("Orange");
+//        list.addFirst("Kiwi");
+//        list.addFirst("Mango");
+//
+//        list.reverse();
+//
+//        assertEquals("Tangerine", list.peekFirst());
+//        assertEquals("Orange", list.get(1));
+//        assertEquals("Kiwi", list.get(2));
+//        assertEquals("Mango", list.peekLast());
+//    }
+//
+//    @Test
+//    @DisplayName("Should use custom/default printer in toString")
+//    void testToString() {
+//        list.addFirst("Soursop");
+//        String standard = list.toString();
+//        String custom = list.toString(p -> ("I like " + p));
+//
+//        assertTrue(standard.contains("[Soursop] -> (head)"));
+//        assertTrue(custom.contains("I like Soursop"));
+//    }
 }
