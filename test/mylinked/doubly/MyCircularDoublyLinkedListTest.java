0ff44b4deeadf14b35e133eeb28f317afc18d5ce
Huy (Randy) <huyvlam@gmail.com>
2026-03-24 17:48:52 -0700

    Test coverage for remove by data using custom comparatpr

--- test/mylinked/doubly/MyCircularDoublyLinkedListTest.java
+++ test/mylinked/doubly/MyCircularDoublyLinkedListTest.java
@@ -1,90 +1,105 @@
 package mylinked.doubly;
 
+import mymodel.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyCircularDoublyLinkedListTest {
-    private MyCircularDoublyLinkedList<Integer> list;
+    private MyCircularDoublyLinkedList<Integer> listI;
+    private MyCircularDoublyLinkedList<Person> listP;
 
     @BeforeEach
     void setUp() {
-        list = new MyCircularDoublyLinkedList<>();
+        listI = new MyCircularDoublyLinkedList<>();
+        listP = new MyCircularDoublyLinkedList<>();
     }
 
     @Test
     @DisplayName("Should add at both ends and update head/tail pointer")
     void testAddFirstLast() {
-        list.addLast(23);
-        list.addFirst(54);
-        list.addLast(89);
+        listI.addLast(23);
+        listI.addFirst(54);
+        listI.addLast(89);
 
-        assertEquals(54, list.peekFirst());
-        assertEquals(89, list.peekLast());
-        assertEquals(3, list.size());
+        assertEquals(54, listI.peekFirst());
+        assertEquals(89, listI.peekLast());
+        assertEquals(3, listI.size());
 
-        list.clear();
+        listI.clear();
 
-        assertNull(list.peekFirst());
-        assertNull(list.peekLast());
+        assertNull(listI.peekFirst());
+        assertNull(listI.peekLast());
     }
 
     @Test
     @DisplayName("Should poll single element at either end")
     void testPollFirstLast() {
-        list.addFirst(34);
-        list.addFirst(65);
-        list.addLast(17);
-        list.addLast(9);
+        listI.addFirst(34);
+        listI.addFirst(65);
+        listI.addLast(17);
+        listI.addLast(9);
 
-        assertEquals(65, list.pollFirst());
-        assertEquals(9, list.pollLast());
-        assertEquals(34, list.pollFirst());
-        assertEquals(17, list.pollLast());
-        assertNull(list.peekFirst());
-        assertEquals(0, list.size());
+        assertEquals(65, listI.pollFirst());
+        assertEquals(9, listI.pollLast());
+        assertEquals(34, listI.pollFirst());
+        assertEquals(17, listI.pollLast());
+        assertNull(listI.peekFirst());
+        assertEquals(0, listI.size());
     }
 
     @Test
     @DisplayName("Should add/remove data by the given index")
     void testAddRemoveByIndex() {
-        list.add(0, 13);
-        list.add(1, 45);
-        list.add(2, 87);
-        list.add(1, 69);
+        listI.add(0, 13);
+        listI.add(1, 45);
+        listI.add(2, 87);
+        listI.add(1, 69);
 
-        assertEquals(13, list.peekFirst());
-        assertEquals(87, list.peekLast());
+        assertEquals(13, listI.peekFirst());
+        assertEquals(87, listI.peekLast());
 
-        assertEquals(45, list.remove(2));
-        assertEquals(69, list.remove(1));
+        assertEquals(45, listI.remove(2));
+        assertEquals(69, listI.remove(1));
 
-        assertEquals(2, list.size());
+        assertEquals(2, listI.size());
     }
 
     @Test
     @DisplayName("Should remove by data and return true for success")
     void testRemoveByData() {
-        list.addLast(75);
-        list.addFirst(21);
-        list.addFirst(64);
+        listI.addLast(75);
+        listI.addFirst(21);
+        listI.addFirst(64);
 
-        assertTrue(list.remove((Integer) 21));
-        assertFalse(list.remove((Integer) 22));
+        assertTrue(listI.remove((Integer) 21));
+        assertFalse(listI.remove((Integer) 22));
+    }
+
+    @Test
+    @DisplayName("Should remove by data using custom comparator")
+    void testRemoveByDataCustomComparator() {
+        listP.addLast(new Person("Jana", 34));
+        listP.addLast(new Person("Yoga", 51));
+
+        assertTrue(listP.remove(new Person("Java", 34), Comparator.comparingInt(p -> p.age)));
+        assertTrue(listP.remove(new Person("Yoga", 26), Comparator.comparing(p -> p.name)));
     }
 
     @Test
     @DisplayName("Should replace/get data by the given index")
     void testGetSetByIndex() {
-        list.addLast(68);
-        list.addLast(49);
-        list.addLast(52);
+        listI.addLast(68);
+        listI.addLast(49);
+        listI.addLast(52);
 
-        assertEquals(49, list.set(1, 37));
-        assertEquals(37, list.get(1));
-        assertEquals(52, list.set(2, 13));
-        assertEquals(13, list.get(2));
+        assertEquals(49, listI.set(1, 37));
+        assertEquals(37, listI.get(1));
+        assertEquals(52, listI.set(2, 13));
+        assertEquals(13, listI.get(2));
     }
 }
