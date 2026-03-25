f0299de6c9ab2f20d065ff6ee02dbfadb8696af1
Huy (Randy) <huyvlam@gmail.com>
2026-03-25 16:05:21 -0700

    Update addLast, pollFirst, pollLast, add by index methods

--- src/mylinked/singly/MyCircularLinkedList.java
+++ src/mylinked/singly/MyCircularLinkedList.java
@@ -33,74 +33,66 @@ public class MyCircularLinkedList<E> {
         return (count == 0) ? null : tail.data;
     }
 
+    private void updateTail(SinglyNode<E> node) {
+        if (tail == null) {
+            tail = node;
+        } else {
+            node.next = tail.next;
+        }
+        tail.next = node;
+    }
+
     public void addFirst(E data) {
         Checker.checkNullArgument(data);
 
         SinglyNode<E> node = new SinglyNode<>(data);
-
-        if (tail == null) {
-            tail = node;
-            tail.next = node;
-        } else {
-            node.next = tail.next;
-            tail.next = node;
-        }
-
+        updateTail(node);
         count++;
     }
 
     public void addLast(E data) {
-        if (data == null) throw new IllegalArgumentException("Data cannot be null");
-
-        if (head == null) {
-            addFirst(data);
-            return;
-        }
+        Checker.checkNullArgument(data);
 
         SinglyNode<E> node = new SinglyNode<>(data);
-        tail.next = node;
-        node.next = head;
+        updateTail(node);
         tail = node;
         count++;
     }
 
     public E pollFirst() {
-        if (head == null) return null;
+        if (count == 0) return null;
 
-        E data = head.data;
+        SinglyNode<E> first = tail.next;
 
-        if (head == tail) {
-            head = null;
-            tail = null;
-        } else {
-            head = head.next;
-            tail.next = head;
-        }
+        if (first == tail) tail = null;
+        else tail.next = first.next;
+
         count--;
-
-        return data;
+        return first.data;
     }
 
     public E pollLast() {
-        if (tail == null) return null;
-
-        if (head == tail) return pollFirst();
+        if (count == 0) return null;
 
         E data = tail.data;
-        SinglyNode<E> prev = head;
+        SinglyNode<E> prev = tail.next;
 
         while (prev.next != tail)
             prev = prev.next;
 
-        prev.next = head;
-        tail = prev;
-        count--;
+        if (prev == tail) {
+            tail = null;
+        } else {
+            prev.next = tail.next;
+            tail = prev;
+        }
 
+        count--;
         return data;
     }
 
     public void add(int i, E data) {
-        if (data == null) throw new IllegalArgumentException("Data cannot be null");
+        Checker.checkNullArgument(data);
         if (i < 0 || i > count) throw new IndexOutOfBoundsException("Index cannot be out of bound");
 
         if (i == 0) {
@@ -112,155 +104,154 @@ public class MyCircularLinkedList<E> {
             return;
         }
 
-        SinglyNode<E> node = new SinglyNode<>(data);
-        SinglyNode<E> prev = head;
-
+        SinglyNode<E> prev = tail.next;
         for (int index = 0; index < i - 1; index++)
             prev = prev.next;
 
+        SinglyNode<E> node = new SinglyNode<>(data);
         node.next = prev.next;
         prev.next = node;
         count++;
     }
 
-    public E remove(int i) {
-        if (i < 0 || i >= count) throw new IndexOutOfBoundsException("Index cannot be out of bound");
-
-        if (i == 0) return pollFirst();
-        if (i == count - 1) return pollLast();
-
-        SinglyNode<E> prev = head;
-
-        for (int index = 0; index < i - 1; index++)
-            prev = prev.next;
-
-        SinglyNode<E> removed = prev.next;
-        E data = removed.data;
-        prev.next = removed.next;
-
-        count--;
-        return data;
-    }
-
-    public boolean remove(E data) {
-        return remove(data, comparator);
-    }
-
-    public boolean remove(E data, Comparator<? super E> comp) {
-        Checker.checkNullArgument(comp, "Comparator");
-        if (data == null || count == 0) return false;
-
-        if (comp.compare(head.data, data) == 0) {
-            pollFirst();
-            return true;
-        }
-        if (comp.compare(tail.data, data) == 0) {
-            pollLast();
-            return true;
-        }
-
-        SinglyNode<E> prev = head;
-
-        while (prev.next != tail) {
-            SinglyNode<E> cur = prev.next;
-            if (comp.compare(cur.data, data) == 0) {
-                prev.next = cur.next;
-                count--;
-
-                return true;
-            }
-            prev = prev.next;
-        }
-        return false;
-    }
-
-    public E set(int i, E data) {
-        if (i < 0 || i >= count) throw new IndexOutOfBoundsException("Index cannot be out of bound");
-        if (data == null) throw new IllegalArgumentException("Data cannot be null");
-
-        SinglyNode<E> cur = head;
-
-        for (int index = 0; index < i; index++)
-            cur = cur.next;
-
-        E replaced = cur.data;
-        cur.data = data;
-
-        return replaced;
-    }
-
-    public E get(int i) {
-        if (i < 0 || i >= count) throw new IndexOutOfBoundsException("Index cannot be out of bound");
-
-        if (i == count - 1) return peekLast();
-
-        SinglyNode<E> cur = head;
-
-        for (int index = 0; index < i; index++)
-            cur = cur.next;
-
-        return cur.data;
-    }
-
-    public int indexOf(E data) {
-        return indexOf(data, comparator);
-    }
-
-    public int indexOf(E data, Comparator<? super E> comp) {
-        Checker.checkNullArgument(comp, "Comparator");
-        if (data == null) return -1;
-
-        SinglyNode<E> cur = head;
-
-        for (int index = 0; index < count; index++) {
-            if (comp.compare(cur.data, data) == 0) return index;
-            cur = cur.next;
-        }
-
-        return -1;
-    }
-
-    public void reverse() {
-        if (head == null || head.next == null) return;
-
-        SinglyNode<E> prev = tail;
-        SinglyNode<E> cur = head;
-        SinglyNode<E> next;
-
-        for (int i = 0; i < count; i++) {
-            next = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = next;
-        }
-
-        tail = head;
-        head = prev;
-    }
-
-    @Override
-    public String toString() {
-        return toString(null);
-    }
-
-    public String toString(Printer<E> printer) {
-        if (head == null) return "CircularLinkedList: [ empty ]";
-
-        Printer<E> safePrinter = (printer == null) ? (Object::toString) : printer;
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("CircularLinkedList (size ").append(count).append("): ");
-
-        SinglyNode<E> cur = head;
-        do {
-            sb.append("[").append(safePrinter.print(cur.data)).append("]");
-
-            if (cur.next != head) sb.append(" -> ");
-            else sb.append(" -> (head)");
-
-            cur = cur.next;
-        } while (cur != head);
-
-        return sb.toString();
-    }
+//    public E remove(int i) {
+//        if (i < 0 || i >= count) throw new IndexOutOfBoundsException("Index cannot be out of bound");
+//
+//        if (i == 0) return pollFirst();
+//        if (i == count - 1) return pollLast();
+//
+//        SinglyNode<E> prev = head;
+//
+//        for (int index = 0; index < i - 1; index++)
+//            prev = prev.next;
+//
+//        SinglyNode<E> removed = prev.next;
+//        E data = removed.data;
+//        prev.next = removed.next;
+//
+//        count--;
+//        return data;
+//    }
+//
+//    public boolean remove(E data) {
+//        return remove(data, comparator);
+//    }
+//
+//    public boolean remove(E data, Comparator<? super E> comp) {
+//        Checker.checkNullArgument(comp, "Comparator");
+//        if (data == null || count == 0) return false;
+//
+//        if (comp.compare(head.data, data) == 0) {
+//            pollFirst();
+//            return true;
+//        }
+//        if (comp.compare(tail.data, data) == 0) {
+//            pollLast();
+//            return true;
+//        }
+//
+//        SinglyNode<E> prev = head;
+//
+//        while (prev.next != tail) {
+//            SinglyNode<E> cur = prev.next;
+//            if (comp.compare(cur.data, data) == 0) {
+//                prev.next = cur.next;
+//                count--;
+//
+//                return true;
+//            }
+//            prev = prev.next;
+//        }
+//        return false;
+//    }
+//
+//    public E set(int i, E data) {
+//        if (i < 0 || i >= count) throw new IndexOutOfBoundsException("Index cannot be out of bound");
+//        if (data == null) throw new IllegalArgumentException("Data cannot be null");
+//
+//        SinglyNode<E> cur = head;
+//
+//        for (int index = 0; index < i; index++)
+//            cur = cur.next;
+//
+//        E replaced = cur.data;
+//        cur.data = data;
+//
+//        return replaced;
+//    }
+//
+//    public E get(int i) {
+//        if (i < 0 || i >= count) throw new IndexOutOfBoundsException("Index cannot be out of bound");
+//
+//        if (i == count - 1) return peekLast();
+//
+//        SinglyNode<E> cur = head;
+//
+//        for (int index = 0; index < i; index++)
+//            cur = cur.next;
+//
+//        return cur.data;
+//    }
+//
+//    public int indexOf(E data) {
+//        return indexOf(data, comparator);
+//    }
+//
+//    public int indexOf(E data, Comparator<? super E> comp) {
+//        Checker.checkNullArgument(comp, "Comparator");
+//        if (data == null) return -1;
+//
+//        SinglyNode<E> cur = head;
+//
+//        for (int index = 0; index < count; index++) {
+//            if (comp.compare(cur.data, data) == 0) return index;
+//            cur = cur.next;
+//        }
+//
+//        return -1;
+//    }
+//
+//    public void reverse() {
+//        if (head == null || head.next == null) return;
+//
+//        SinglyNode<E> prev = tail;
+//        SinglyNode<E> cur = head;
+//        SinglyNode<E> next;
+//
+//        for (int i = 0; i < count; i++) {
+//            next = cur.next;
+//            cur.next = prev;
+//            prev = cur;
+//            cur = next;
+//        }
+//
+//        tail = head;
+//        head = prev;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return toString(null);
+//    }
+//
+//    public String toString(Printer<E> printer) {
+//        if (head == null) return "CircularLinkedList: [ empty ]";
+//
+//        Printer<E> safePrinter = (printer == null) ? (Object::toString) : printer;
+//
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("CircularLinkedList (size ").append(count).append("): ");
+//
+//        SinglyNode<E> cur = head;
+//        do {
+//            sb.append("[").append(safePrinter.print(cur.data)).append("]");
+//
+//            if (cur.next != head) sb.append(" -> ");
+//            else sb.append(" -> (head)");
+//
+//            cur = cur.next;
+//        } while (cur != head);
+//
+//        return sb.toString();
+//    }
 }
