f7e3f513acf41626a6b3a33feb597b5df60f6d15
Huy (Randy) <huyvlam@gmail.com>
2026-03-26 22:25:38 -0700

    Implement remove by index, trim to size methods

--- src/myarray/MyArrayList.java
+++ src/myarray/MyArrayList.java
@@ -1,37 +1,62 @@
 package myarray;
 
+import myhelper.Checker;
+
 import java.util.Arrays;
 
 public class MyArrayList<E> {
-    private Object[] arr;
+    private Object[] arrList;
 
     private final int DEFAULT_CAPACITY;
     private int size;
 
     public MyArrayList(int capacity) {
         DEFAULT_CAPACITY = (capacity == 0) ? 5 : capacity;
-        arr = new Object[DEFAULT_CAPACITY];
+        arrList = new Object[DEFAULT_CAPACITY];
         size = 0;
     }
 
     public int capacity() {
-        return arr.length;
+        return arrList.length;
     }
 
     public int size() {
         return size;
     }
 
-    public void add(E data) {
-        if (size == arr.length) grow();
-        arr[size++] = data;
+    private void grow() {
+        int curCap = arrList.length;
+
+        if (size < curCap) return;
+
+        int newCap = curCap + (curCap >> 1);
+        // ensure new capacity grow by at least 1 for edge case
+        newCap = (newCap <= curCap) ? curCap + 1 : newCap;
+
+        arrList = Arrays.copyOf(arrList, newCap);
     }
 
-    public void grow() {
-        int newCapacity = size + (size >> 1);
+    public void add(E data) {
+        grow();
+        arrList[size++] = data;
+    }
 
-        if (newCapacity <= size) newCapacity = size + 1;
+    public E remove(int i) {
+        Checker.checkBounds(i, size);
 
-        arr = Arrays.copyOf(arr, newCapacity);
+        E removed = (E) arrList[i];
+
+        int numShift = size - i - 1;
+        if (numShift > 0)
+            System.arraycopy(arrList, i + 1, arrList, i, numShift);
+
+        arrList[--size] = null;
+
+        return removed;
+    }
+
+    public void trimToSize() {
+        if (size < arrList.length)
+            arrList = (size == 0) ? new Object[DEFAULT_CAPACITY] : Arrays.copyOf(arrList, size);
     }
 }
