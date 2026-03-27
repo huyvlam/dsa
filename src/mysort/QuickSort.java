841eef730447d73079316eeaf76b82b8feeed8a9
Huy (Randy) <huyvlam@gmail.com>
2026-03-27 11:36:22 -0700

    Implement quick sort method. Add test coverage for quick sort method

---
+++ src/mysort/QuickSort.java
@@ -0,0 +1,63 @@
+package mysort;
+
+import myhelper.Checker;
+
+import java.util.Comparator;
+
+public class QuickSort<T extends Comparable<T>> {
+    public static <T> void sort(T[] arr, Comparator<? super T> comp) {
+        Checker.checkNullArgument(comp);
+
+        int n = arr.length;
+        if (n <= 1) return;
+
+        sort(arr, 0, n - 1, comp);
+    }
+
+    private static <T> void sort(T[] arr, int lo, int hi,  Comparator<? super T> comp) {
+        // 1. Partition the array until it becomes a single element
+        if (lo < hi) {
+            // Note: Pivot point is equivalent to the root in red black tree
+            int p = partition(arr, lo, hi, comp);
+
+            // 2. Use returned pivot point to recurse sorting left/right half
+            sort(arr, lo, p - 1, comp); // smaller elements
+            sort(arr, p + 1, hi, comp); // larger elements
+        }
+    }
+
+    private static <T> int partition(T[] arr, int lo, int hi, Comparator<? super T> comp) {
+        Comparator<? super T> safeComp = Comparator.nullsLast(comp);
+
+        // 3. We use the last element as pivot
+        T pivotElement = arr[hi];
+
+        // 4. Initialize the position of swapped element (we start out of bound since no swap occurs initially)
+        int swapIndex = lo - 1;
+
+        // 5. Iterate left to right
+        for (int j = lo; j < hi; j++) {
+            // 6. If current element is the winner, pivot to the left
+            if (safeComp.compare(arr[j], pivotElement) < 0) {
+                swapIndex++; // move swap position forward
+
+                T temp = arr[swapIndex];
+
+                // Move the winner into swap position
+                arr[swapIndex] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int pivotIndex = swapIndex + 1;
+        T temp = arr[pivotIndex];
+
+        // 5. Move pivot element right behind the last element that was swapped
+        // All elements left of pivot are smaller, right of pivot are larger
+        arr[pivotIndex] = arr[hi];
+        arr[hi] = temp;
+
+        // 6. Return the pivot position to use as starting point for subsequent partition
+        return pivotIndex;
+    }
+}
---
+++ test/mysort/QuickSortTest.java
@@ -0,0 +1,52 @@
+package mysort;
+
+import mymodel.Person;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class QuickSortTest {
+    private String[] arrS;
+    private Person[] arrP;
+    private Comparator<String> naturalComp;
+    private Comparator<Person> customComp;
+
+    @BeforeEach
+    void setUp() {
+        naturalComp = Comparator.naturalOrder();
+        customComp = Comparator.comparing((p) -> p.age);
+
+        arrS = new String[]{"soccer","archery",null,"rowing","baseball","volleyball","judo","sepak takraw"};
+        arrP = new Person[]{
+                null,
+                new Person("Yuri", 34),
+                new Person("June", 21),
+                new Person("Ha", 56),
+                new Person("Domo", 17),
+                new Person("Timu", 48)
+        };
+    }
+
+    @Test
+    @DisplayName("Should sort elements using natural order")
+    void testSortNaturalOrder() {
+        QuickSort.sort(arrS, naturalComp);
+        assertEquals("archery", arrS[0]);
+        assertEquals("volleyball", arrS[arrS.length - 2]);
+        assertNull(arrS[arrS.length - 1]);
+    }
+
+    @Test
+    @DisplayName("Should sort elements using custom comparator")
+    void testSortCustom() {
+        QuickSort.sort(arrP, customComp);
+        assertEquals("Domo", arrP[0].name);
+        assertEquals("Ha", arrP[arrP.length - 2].name);
+        assertNull(arrP[arrP.length - 1]);
+    }
+}
