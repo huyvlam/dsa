5a8dac457c250cc1f205dab7e627c46b4c488cda
Huy (Randy) <huyvlam@gmail.com>
2026-03-27 09:18:13 -0700

    Implement findIndex method

--- src/mysearch/LinearSearch.java
+++ src/mysearch/LinearSearch.java
@@ -2,14 +2,49 @@ package mysearch;
 
 import java.util.Comparator;
 
+/**
+ * Pairwise comparison linear search for unsorted array
+ */
 public class LinearSearch {
     /**
-     * Pairwise comparison search for min/max values
+     * Find the index of given element in the array
+     * @param arr array of elements to be searched
+     * @param value target element to search for
+     * @param comp custom comparator or Comparator.naturalOrder
+     * @return the index of given element, or -1 if not found
+     * @param <T> type of element
+     */
+    public static <T> int findIndex(T[] arr, T value, Comparator<? super T> comp) {
+        int n = arr.length;
+        if (n == 0) return -1;
+
+        int i;
+
+        if (n % 2 == 1) {
+            if (arr[0] != null && comp.compare(arr[0], value) == 0) return 0;
+
+            i = 1;
+        } else {
+            i = 0;
+        }
+
+        while (i < n - 1) {
+            if (arr[i] != null && comp.compare(arr[i], value) == 0) return i;
+            if (arr[i + 1] != null && comp.compare(arr[i + 1], value) == 0) return i + 1;
+
+            i += 2;
+        }
+
+        return -1;
+    }
+
+    /**
+     * Search for min/max elements in the given array
      * @param arr array of elements to search
      * @param comp custom comparator for generic elements
-     *             elements w/ natural ordering can use Comparator.naturalOrder()
+     *             Comparator.naturalOrder() may be used for elements w/ natural ordering
      * @return Pair<T> with min/max field
-     * @param <T> type of element being searched
+     * @param <T> type of element
      */
     public static <T> Pair<T> findMinMax(T[] arr, Comparator<? super T> comp) {
         int n = arr.length;
