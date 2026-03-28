82c53389aa06b6fda49842a3728aa2d6838afd73
Huy (Randy) <huyvlam@gmail.com>
2026-03-28 14:40:11 -0700

    Adding safe comparator to handle null comp argument

--- src/mysearch/LinearSearch.java
+++ src/mysearch/LinearSearch.java
@@ -1,5 +1,7 @@
 package mysearch;
 
+import myhelper.Checker;
+
 import java.util.Comparator;
 
 /**
@@ -9,25 +11,28 @@ public class LinearSearch {
     /**
      * Find the index of given element in the array
      * @param arr array of elements to be searched
-     * @param value target element to search for
-     * @param comp custom comparator or Comparator.naturalOrder
+     * @param value target element
+     * @param comp custom comparator, if null the method resolve to natural order
      * @return the index of given element, or -1 if not found
-     * @param <T> type of element
+     * @param <E> type of element
      */
-    public static <T> int findIndex(T[] arr, T value, Comparator<? super T> comp) {
+    public static <E> int findIndex(E[] arr, E value, Comparator<? super E> comp) {
+        if (arr == null || value == null) return -1;
+
         int n = arr.length;
         if (n == 0) return -1;
 
+        Comparator<? super E> safeComp = Checker.nullsLastComparator(comp);
         int i = 0;
 
         if (n % 2 == 1) {
-            if (arr[0] != null && comp.compare(arr[0], value) == 0) return 0;
+            if (arr[0] != null && safeComp.compare(arr[0], value) == 0) return 0;
             i = 1;
         }
 
         while (i < n - 1) {
-            if (arr[i] != null && comp.compare(arr[i], value) == 0) return i;
-            if (arr[i + 1] != null && comp.compare(arr[i + 1], value) == 0) return i + 1;
+            if (arr[i] != null && safeComp.compare(arr[i], value) == 0) return i;
+            if (arr[i + 1] != null && safeComp.compare(arr[i + 1], value) == 0) return i + 1;
 
             i += 2;
         }
@@ -41,18 +46,21 @@ public class LinearSearch {
      * @param comp custom comparator for generic elements
      *             Comparator.naturalOrder() may be used for elements w/ natural ordering
      * @return Pair<T> with min/max field
-     * @param <T> type of element
+     * @param <E> type of element
      */
-    public static <T> Pair<T> findMinMax(T[] arr, Comparator<? super T> comp) {
+    public static <E> Pair<E> findMinMax(E[] arr, Comparator<? super E> comp) {
+        if (arr == null) return null;
+
         int n = arr.length;
         if (n == 0) return null;
 
-        Pair<T> result = new Pair<>();
+        Comparator<? super E> safeComp = Checker.nullsLastComparator(comp);
+        Pair<E> result = new Pair<>();
         int i;
 
-        // initialize min/max
+        // Initialize min/max
         if (n % 2 == 0) { // if number of elements are even
-            if (comp.compare(arr[0], arr[1]) > 0) {
+            if (safeComp.compare(arr[0], arr[1]) > 0) {
                 result.max = arr[0];
                 result.min = arr[1];
             } else {
@@ -68,15 +76,15 @@ public class LinearSearch {
 
         // compare in pair
         while (i < n - 1) {
-            if (comp.compare(arr[i], arr[i + 1]) > 0) {
-                if (comp.compare(arr[i], result.max) > 0)
+            if (safeComp.compare(arr[i], arr[i + 1]) > 0) {
+                if (safeComp.compare(arr[i], result.max) > 0)
                     result.max = arr[i];
-                if (comp.compare(arr[i+ 1], result.min) < 0)
+                if (safeComp.compare(arr[i+ 1], result.min) < 0)
                     result.min = arr[i + 1];
             } else {
-                if (comp.compare(arr[i + 1], result.max) > 0)
+                if (safeComp.compare(arr[i + 1], result.max) > 0)
                     result.max = arr[i + 1];
-                if (comp.compare(arr[i], result.min) < 0)
+                if (safeComp.compare(arr[i], result.min) < 0)
                     result.min = arr[i];
             }
             i += 2;
