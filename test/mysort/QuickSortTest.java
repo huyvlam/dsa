23637ed49d16e40b5ba75a15c62a399668491449
Huy (Randy) <huyvlam@gmail.com>
2026-03-28 13:14:34 -0700

    Test coverage for nulls last

--- test/mysort/QuickSortTest.java
+++ test/mysort/QuickSortTest.java
@@ -13,16 +13,16 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 class QuickSortTest {
     private String[] arrS;
     private Person[] arrP;
-    private Comparator<Person> customComp;
+    private Comparator<Person> comp;
 
     @BeforeEach
     void setUp() {
-        customComp = Comparator.comparing((p) -> p.age);
+        comp = Comparator.comparing((p) -> p.age);
 
         arrS = new String[]{"soccer","archery",null,"rowing","baseball","volleyball","judo","sepak takraw"};
         arrP = new Person[]{
-                null,
                 new Person("Yuri", 34),
+                null,
                 new Person("June", 21),
                 new Person("Ha", 56),
                 new Person("Domo", 17),
@@ -31,20 +31,33 @@ class QuickSortTest {
     }
 
     @Test
-    @DisplayName("Should sort elements using natural order")
-    void testSortNaturalOrder() {
-        QuickSort.sort(arrS, 0, arrS.length - 1, null);
+    @DisplayName("Should sort Comparable elements using natural order")
+    void testSortStandard() {
+        int n = arrS.length;
+        QuickSort.sort(arrS, 0, n - 1, null);
         assertEquals("archery", arrS[0]);
-        assertEquals("volleyball", arrS[arrS.length - 2]);
-        assertNull(arrS[arrS.length - 1]);
+        assertEquals("volleyball", arrS[n - 2]);
     }
 
     @Test
-    @DisplayName("Should sort elements using custom comparator")
+    @DisplayName("Should move null to the end of array")
+    void testNullsLast() {
+        int n = arrS.length;
+        QuickSort.sort(arrS, 0, n - 1, null);
+        assertNull(arrS[n - 1]);
+
+        int m = arrP.length;
+        QuickSort.sort(arrP, 0, m - 1, comp);
+        assertNull(arrP[m - 1]);
+    }
+
+    @Test
+    @DisplayName("Should sort generic elements using custom comparator")
     void testSortCustom() {
-        QuickSort.sort(arrP, 0, arrP.length - 1, customComp);
+        int n = arrP.length;
+        QuickSort.sort(arrP, 0, n - 1, comp);
         assertEquals("Domo", arrP[0].name);
-        assertEquals("Ha", arrP[arrP.length - 2].name);
+        assertEquals("Ha", arrP[n - 2].name);
         assertNull(arrP[arrP.length - 1]);
     }
 }
