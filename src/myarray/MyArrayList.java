cbba3fe48258bc15d319acdc7cdf239f7bd0f7a1
Huy (Randy) <huyvlam@gmail.com>
2026-03-29 14:18:11 -0700

    Refactor index of, sort methods to use MyComparator helper class

--- src/myarray/MyArrayList.java
+++ src/myarray/MyArrayList.java
@@ -1,13 +1,13 @@
 package myarray;
 
 import myhelper.Checker;
+import myhelper.MyComparator;
 import mysearch.BinarySearch;
 import mysearch.LinearSearch;
 import mysort.QuickSort;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Objects;
 
 public class MyArrayList<E> {
     private Object[] dataList;
@@ -77,34 +77,24 @@ public class MyArrayList<E> {
     }
 
     public int indexOf(E data) {
-        return indexOf(data, null);
+        return indexOf(data, MyComparator.equalsComparator);
     }
 
     public int indexOf(E data, Comparator<? super E> comp) {
         Checker.checkNullArgument(data);
 
-        if (comp == null) {
-            comp = Checker.isComparable(dataList, size)
-                    ? Checker.nullsLastComparator(null)
-                    : (a, b) -> Objects.equals(a, b) ? 0 : -1;
-        }
-
-        return sorted ?
-                BinarySearch.findIndex((E[]) dataList, data, 0, size - 1, comp) :
-                LinearSearch.findIndex((E[]) dataList, data, comp);
+        return sorted
+                ? BinarySearch.findIndex((E[]) dataList, data, 0, size - 1, MyComparator.nullsLastComparator(comp))
+                : LinearSearch.findIndex((E[]) dataList, data, MyComparator.nullsLastComparator(comp));
     }
 
     public void sort(Comparator<? super E> comp) {
         if (size <= 1) return;
 
-        if (comp == null) {
-            if (Checker.isComparable(dataList, size))
-                comp = Checker.nullsLastComparator(null);
-            else
-                throw new IllegalArgumentException("Comparator cannot be null for non-Comparable elements");
-        }
+        if (comp == null && !MyComparator.isComparable(dataList, size))
+            throw new IllegalArgumentException("Comparator cannot be null for non-Comparable elements");
 
-        QuickSort.sort((E[]) dataList, 0, size - 1, comp);
+        QuickSort.sort((E[]) dataList, 0, size - 1, MyComparator.nullsLastComparator(comp));
         sorted = true;
     }
 }
