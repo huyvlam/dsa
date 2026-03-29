a9e8c83062d17fa03fb70ab3c42091077bdff517
Huy (Randy) <huyvlam@gmail.com>
2026-03-29 14:15:34 -0700

    Adding instructive comments

--- src/mysearch/BinarySearch.java
+++ src/mysearch/BinarySearch.java
@@ -5,6 +5,17 @@ import myhelper.MyComparator;
 import java.util.Comparator;
 
 public class BinarySearch {
+    /**
+     * Find the index of given data in the sorted array
+     *
+     * @param arr   array of sorted elements to be searched
+     * @param value target element to look for
+     * @param lo    the lower bound must not be less than 0
+     * @param hi    the upper bound must be less than the array size
+     * @param comp  custom comparator for generics, or natural order for Comparables
+     * @return      the position where the element is found, or -1 if not found
+     * @param <E>   type of elements stored in the array
+     */
     public static <E> int findIndex(E[] arr, E value, int lo, int hi, Comparator<? super E> comp) {
         Comparator<? super E> safeComp = MyComparator.nullsLastComparator(comp);
 
