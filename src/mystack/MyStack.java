f78ca6cfd9734d87f84a3a45c24aefe41e904890
Huy (Randy) <huyvlam@gmail.com>
2026-03-29 22:10:09 -0700

    Rename to arlist

--- src/mystack/MyStack.java
+++ src/mystack/MyStack.java
@@ -5,33 +5,33 @@ import myarray.MyArrayList;
 import java.util.EmptyStackException;
 
 public class MyStack<E> {
-    private final MyArrayList<E> stack;
+    private final MyArrayList<E> arlist;
 
     public MyStack() {
-        stack = new MyArrayList<>();
+        arlist = new MyArrayList<>();
     }
 
     public boolean empty() {
-        return stack.size() == 0;
+        return arlist.size() == 0;
     }
 
     public E peek() {
         if (empty()) throw new EmptyStackException();
-        return stack.get(stack.size() - 1);
+        return arlist.get(arlist.size() - 1);
     }
 
     public E pop() {
         if (empty()) throw new EmptyStackException();
-        return stack.remove(stack.size() - 1);
+        return arlist.remove(arlist.size() - 1);
     }
 
     public E push(E data) {
-        stack.add(data);
+        arlist.add(data);
         return data;
     }
 
     public int search(E data) {
-        int index = stack.indexOf(data);
-        return index == -1 ? -1 : stack.size() - index;
+        int index = arlist.indexOf(data);
+        return index == -1 ? -1 : arlist.size() - index;
     }
 }
