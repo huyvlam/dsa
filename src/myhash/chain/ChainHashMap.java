16ddad5986dc6a444f827d96a5cbfe738b09aae9
Huy (Randy) <huyvlam@gmail.com>
2026-03-31 07:34:41 -0700

    Create chaining hash map. Implement core methods: put, get, remove, resize

---
+++ src/myhash/chain/ChainHashMap.java
@@ -0,0 +1,119 @@
+package myhash.chain;
+
+import myhash.HashHelper;
+
+public class ChainHashMap<K, V> {
+    private ChainHashNode<K, V>[] table;
+    private int size;
+    private final int initialCapacity;
+    private final double loadFactor;
+
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
+
+    public ChainHashMap(int capacity, double factor) {
+        if (capacity <= 0 || (capacity & (capacity - 1)) != 0) throw new IllegalArgumentException("Capacity must be power of 2");
+
+        initialCapacity = capacity;
+        loadFactor = factor;
+        table = new ChainHashNode[initialCapacity];
+    }
+
+    public ChainHashMap(int capacity) {
+        this(capacity, DEFAULT_LOAD_FACTOR);
+    }
+
+    public ChainHashMap() {
+        this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR);
+    }
+
+    public void clear() {
+        table = new ChainHashNode[initialCapacity];
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void put(K key, V value) {
+        int index = HashHelper.hashIndex(key, table.length);
+
+        ChainHashNode<K, V> head = table[index];
+        ChainHashNode<K, V> cur = head;
+
+        while (cur != null) {
+            if (HashHelper.areEqualKeys(cur, key)) {
+                cur.value = value;
+                return;
+            }
+            cur = cur.next;
+        }
+
+        ChainHashNode<K, V> node = new ChainHashNode<>(key, value, head);
+        table[index] = node;
+        size++;
+
+        if (HashHelper.needsResize(size, table.length, loadFactor)) resize();
+    }
+
+    public void resize() {
+        int newCapacity = table.length * 2;
+        ChainHashNode<K, V>[] newEntries = new ChainHashNode[newCapacity];
+
+        for (int i = 0; i < table.length; i++) {
+            ChainHashNode<K, V> cur = table[i];
+
+            while (cur != null) {
+                ChainHashNode<K, V> next = cur.next;
+
+                int index = HashHelper.hashIndex(cur.key, newCapacity);
+
+                cur.next = newEntries[index];
+                newEntries[index] = cur;
+
+                cur = next;
+            }
+        }
+
+        table = newEntries;
+    }
+
+    public V get(K key) {
+        int index = HashHelper.hashIndex(key, table.length);
+        ChainHashNode<K, V> cur = table[index];
+
+        while (cur != null) {
+            if (HashHelper.areEqualKeys(cur, key)) return cur.value;
+
+            cur = cur.next;
+        }
+
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = HashHelper.hashIndex(key, table.length);
+        ChainHashNode<K, V> cur = table[index];
+        ChainHashNode<K, V> prev = null;
+
+        while (cur != null) {
+            if (HashHelper.areEqualKeys(cur, key)) {
+                V removed = cur.value;
+
+                if (prev == null) table[index] = cur.next;
+                else prev.next = cur.next;
+
+                size--;
+                return removed;
+            }
+            prev = cur;
+            cur = cur.next;
+        }
+        return null;
+    }
+}
