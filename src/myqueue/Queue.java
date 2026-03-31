89658a121dbb9d8bf84c54c2c9170f53e34c1b59
Huy (Randy) <huyvlam@gmail.com>
2026-03-31 06:51:53 -0700

    Add clear, size, is empty methods

--- src/myqueue/Queue.java
+++ src/myqueue/Queue.java
@@ -5,45 +5,57 @@ import mylinked.singly.SinglyCircularLinkedList;
 import java.util.NoSuchElementException;
 
 public class Queue<E> {
-    private final SinglyCircularLinkedList<E> cslist;
+    private final SinglyCircularLinkedList<E> sclist;
     private final int CAPACITY;
 
     public Queue(int capacity) {
-        cslist = new SinglyCircularLinkedList<>();
+        sclist = new SinglyCircularLinkedList<>();
         CAPACITY = capacity;
     }
 
-    public boolean add(E data) {
-        if (cslist.size() == CAPACITY) throw new IllegalStateException("No available space");
+    public void clear() {
+        sclist.clear();
+    }
 
-        cslist.addLast(data);
+    public int size() {
+        return sclist.size();
+    }
+
+    public boolean isEmpty() {
+        return sclist.size() == 0;
+    }
+
+    public boolean add(E data) {
+        if (sclist.size() == CAPACITY) throw new IllegalStateException("No available space");
+
+        sclist.addLast(data);
         return true;
     }
 
     public boolean offer(E data) {
-        if (cslist.size() == CAPACITY) return false;
+        if (sclist.size() == CAPACITY) return false;
 
-        cslist.addLast(data);
+        sclist.addLast(data);
         return true;
     }
 
     public E peek() {
-        return cslist.peekFirst();
+        return sclist.peekFirst();
     }
 
     public E element() {
-        if (cslist.size() == 0) throw new NoSuchElementException("Queue is empty");
+        if (sclist.size() == 0) throw new NoSuchElementException("Queue is empty");
 
-        return cslist.peekFirst();
+        return sclist.peekFirst();
     }
 
     public E poll() {
-        return cslist.pollFirst();
+        return sclist.pollFirst();
     }
 
     public E remove() {
-        if (cslist.size() == 0) throw new NoSuchElementException("Queue is empty");
+        if (sclist.size() == 0) throw new NoSuchElementException("Queue is empty");
 
-        return cslist.pollFirst();
+        return sclist.pollFirst();
     }
 }
