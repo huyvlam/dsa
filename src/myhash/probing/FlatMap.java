07e72a5e3d0a01b848575e3a3303b6fb593170fe
Huy (Randy) <huyvlam@gmail.com>
2026-04-02 10:59:18 -0700

    Update per refactored changes in flat util

---
+++ src/myhash/probing/FlatMap.java
@@ -0,0 +1,148 @@
+package myhash.probing;
+
+import java.util.Objects;
+
+public class FlatMap<K, V> {
+    public enum Probe { LINEAR, QUADRATIC, DOUBLE }
+
+    private FlatNode<K, V>[] table;
+    private double resizeThreshold;
+    private int size;
+
+    private final ProbeStrategy strategy;
+    private final int INIT_CAPACITY;
+    private final double LOAD_FACTOR;
+
+    private static final int DEFAULT_CAPACITY = 16;
+
+    public FlatMap(Probe probe, int capacity, double factor) {
+        if (capacity <= 0 || (capacity & (capacity - 1)) != 0) throw new IllegalArgumentException("Capacity must be a power of 2");
+
+        INIT_CAPACITY = capacity;
+        LOAD_FACTOR = factor > 0 ? factor : getLoadFactor(probe);
+        this.strategy = getProbeStrategy(probe);
+        table = new FlatNode[INIT_CAPACITY];
+        resizeThreshold = table.length * LOAD_FACTOR;
+        size = 0;
+    }
+
+    public FlatMap(Probe probe, int capacity) {
+        this(probe, capacity, 0);
+    }
+
+    public FlatMap(Probe probe) {
+        this(probe, DEFAULT_CAPACITY, 0);
+    }
+
+    public void clear() {
+        table = new FlatNode[INIT_CAPACITY];
+        resizeThreshold = table.length * LOAD_FACTOR;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void resize() {
+        int newCapacity = table.length * 2;
+        FlatNode<K, V>[] newTable = (FlatNode<K, V>[]) new FlatNode[newCapacity];
+
+        for (FlatNode<K, V> cur : table) {
+            // Filter out deleted nodes for garbage collection (Tombstone Purging)
+            if (cur != null && !cur.deleted) {
+                FlatUtil.probe(cur.key, cur.value, newTable, strategy);
+            }
+        }
+
+        this.table = newTable;
+        this.resizeThreshold = table.length * LOAD_FACTOR;
+    }
+
+    public V put(K key, V value) {
+        if (size >= resizeThreshold) resize();
+
+        V result = FlatUtil.probe(key, value, table, strategy);
+
+        if (result == null) size++;
+
+        return result;
+    }
+
+    public V get(K key) {
+        final V[] result = (V[]) new Object[]{null};
+
+        FlatUtil.probe(key, table, strategy, (node) -> {
+            if (!node.deleted && Objects.equals(node.key, key)) {
+                result[0] = node.value;
+
+                return false;
+            };
+            return true;
+        });
+
+        return (V) result[0];
+    }
+
+    public V remove(K key) {
+        final V[] removed = (V[]) new Object[]{null};
+
+        FlatUtil.probe(key, table, strategy, (node) -> {
+            if (!node.deleted && Objects.equals(node.key, key)) {
+                removed[0] = node.value;
+                node.key = null;
+                node.value = null;
+                node.deleted = true;
+                size--;
+
+                return false; // stopping the loop
+            }
+            return true;
+        });
+
+        return (V) removed[0];
+    }
+
+    public boolean containsKey(K key) {
+        return get(key) != null;
+    }
+
+    public boolean containsValue(V  value) {
+        for (FlatNode<K, V> node : table) {
+            if (node != null && !node.deleted && Objects.equals(node.value, value)) return true;
+        }
+
+        return false;
+    }
+
+    private static ProbeStrategy getProbeStrategy(Probe probe) {
+        switch (probe) {
+            case LINEAR:
+                return FlatUtil.LINEAR;
+            case QUADRATIC:
+                return FlatUtil.QUADRATIC;
+            case DOUBLE:
+                return FlatUtil.DOUBLE;
+            default:
+                throw new IllegalArgumentException("Unsupported probe strategy: " + probe);
+        }
+    }
+
+    private static double getLoadFactor(Probe probe) {
+        switch(probe) {
+            case LINEAR:
+                return 0.7;
+            case QUADRATIC:
+                return 0.5;
+            case DOUBLE:
+                return 0.75;
+            default:
+                throw new IllegalArgumentException("Unsupported probe strategy: " + probe);
+        }
+    }
+}
---
+++ test/myhash/probing/FlatMapTest.java
@@ -0,0 +1,74 @@
+package myhash.probing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlatMapTest<K, V> {
+    private FlatMap<K, V> map;
+    private int initCap;
+
+    @BeforeEach
+    void setUp() {
+        initCap = 4;
+        map = new FlatMap<>(FlatMap.Probe.LINEAR, initCap);
+    }
+
+    @Test
+    @DisplayName("Should throw exception when capacity is not power of 2")
+    void testCapacityNotPowerOf2() {
+        assertThrows(IllegalArgumentException.class, () -> new FlatMap<>(FlatMap.Probe.LINEAR, 0));
+        assertThrows(IllegalArgumentException.class, () -> new FlatMap<>(FlatMap.Probe.LINEAR, 3));
+    }
+
+    @Test
+    @DisplayName("Should put and get data by key")
+    void testPutGetData() {
+        assertNull(map.put((K) "name", (V) "nelly"));
+        assertEquals("nelly", map.put((K) "name", (V) "molly"));
+
+        assertEquals("molly", map.get((K) "name"));
+        assertEquals(1, map.size());
+    }
+
+    @Test
+    @DisplayName("Should remove data by key")
+    void testRemoveData() {
+        map.put((K) "name", (V) "nelly");
+
+        assertEquals("nelly", map.remove((K) "name"));
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Should resize table as needed")
+    void testResizeTable() {
+        map.put((K) "name", (V) "nelly");
+        map.put((K) "age", (V) "20");
+        map.put((K) "hobby", (V) "yoga");
+        map.put((K) "hair", (V) "black");
+        map.put((K) "eyes", (V) "hazel");
+
+        assertTrue(map.size() > initCap);
+
+        map.clear();
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Should check whether the table contains key/value")
+    void testContainsKeyValue() {
+        map.put((K) "name", (V) "nelly");
+        map.put((K) "age", (V) "20");
+        map.put((K) "hair", (V) "black");
+        map.put((K) "eyes", (V) "black");
+
+        assertTrue(map.containsKey((K) "hair"));
+        assertFalse(map.containsKey((K) "hobby"));
+
+        assertTrue(map.containsValue((V) "black"));
+        assertFalse(map.containsValue((V) "brown"));
+    }
+}
