3ca44d1f5bd71fc1756e9b59c8927cb8377336a0
Huy (Randy) <huyvlam@gmail.com>
2026-04-05 12:16:39 -0700

    Add linear probe, quadratic probe, double hash methods

--- src/main/java/myhash/HashUtil.java
+++ src/main/java/myhash/HashUtil.java
@@ -24,6 +24,33 @@ public class HashUtil {
         return hash & (tableSize - 1);
     }
 
+    public static int linearProbe(int orig, int gap, int size) {
+        return (orig + gap) & (size - 1);
+    }
+
+    public static int quadraticProbe(int orig, int gap, int size) {
+        return (orig + gap * gap) & (size - 1);
+    }
+
+    public static int doubleHash(int orig, int gap, int size, int stride) {
+        return (orig + gap * stride) & (size - 1);
+    }
+
+    /**
+     * Compute a positive odd number for Power of 2 table size
+     *
+     * @param key   the key to be converted
+     * @return      a prime number to use in conjunction with double hash method
+     * @param <K>   key type
+     */
+    public static <K> int stride(K key) {
+        int hash = key == null ? 0 : key.hashCode();
+
+        // & 0x7FFFFFFF clears sign bit and ensure positive
+        // | 1 ensure the number is odd
+        return ((hash & 0x7FFFFFFF) % 32) | 1;
+    }
+
     /**
      * Check and return a Power of 2 table size
      *
@@ -41,20 +68,5 @@ public class HashUtil {
         cap |= cap >>> 16;
 
         return (cap < 0) ? 1 : cap + 1;
-    }
-
-    /**
-     * Compute a positive odd number for Power of 2 table size
-     *
-     * @param key   the key to be converted
-     * @return      a prime number to use in conjunction with double hash method
-     * @param <K>   key type
-     */
-    public static <K> int stride(K key) {
-        int hash = key == null ? 0 : key.hashCode();
-
-        // & 0x7FFFFFFF clears sign bit and ensure positive
-        // | 1 ensure the number is odd
-        return ((hash & 0x7FFFFFFF) % 32) | 1;
     }
 }
