db53ba54a31ab1208772232670f7bb99b6b1e015
Huy (Randy) <huyvlam@gmail.com>
2026-04-18 10:50:05 -0700

    Create array base circular queue. Implement base methods: add, offer, poll, remove, peek, element

---
+++ src/main/java/myqueue/CircularArrayQueue.java
@@ -0,0 +1,87 @@
+package myqueue;
+
+import java.util.NoSuchElementException;
+
+public class CircularArrayQueue<E> {
+    private E[] queue;
+    private int capacity;
+    private int size;
+    private int front;
+    private int end;
+
+    public CircularArrayQueue(int capacity) {
+        this.capacity = capacity;
+        queue = (E[]) new Object[capacity];
+        size = 0;
+        front = 0;
+        end = -1;
+    }
+
+    public void clear() {
+        queue = (E[]) new Object[capacity];
+        size = 0;
+        front = 0;
+        end = -1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean add(E value) {
+        if (size == queue.length) throw new IllegalStateException("Capacity is reached");
+
+        end = (end + 1) % queue.length;
+        queue[end] = value;
+        size++;
+        return true;
+    }
+
+    public boolean offer(E value) {
+        if (size == queue.length) return false;
+
+        end = (end + 1) % queue.length;
+        queue[end] = value;
+        size++;
+        return true;
+    }
+
+    public E poll() {
+        if (size == 0) return null;
+
+        E polled = queue[front];
+        front = (front + 1) % queue.length;
+        size--;
+        return polled;
+    }
+
+    public E remove() {
+        if (front == 0) throw new NoSuchElementException("Queue is empty");
+
+        E removed = queue[front];
+        front = (front + 1) % queue.length;
+        size--;
+        return removed;
+    }
+
+    public E peek() {
+        return size == 0 ? null : queue[front];
+    }
+
+    public E element() {
+        if (size == 0) throw new NoSuchElementException("Queue is empty");
+
+        return queue[front];
+    }
+
+    public boolean contains(E value) {
+        for (int i = 0; i < size; i++) {
+            if (queue[i] == value) return true;
+        }
+        return false;
+    }
+}
