12477a6d36d2205da43e950ea71dbb3cd343e44c
Huy (Randy) <huyvlam@gmail.com>
2026-04-22 14:29:47 -0700

    Implement more methods: sort, toString, sync, close

--- src/main/java/mytree/HeapView.java
+++ src/main/java/mytree/HeapView.java
@@ -3,14 +3,23 @@ package mytree;
 import myheap.HeapUtil;
 
 import java.util.ConcurrentModificationException;
-import java.util.NoSuchElementException;
 
 public abstract class HeapView implements AutoCloseable {
     ArrayBinaryTree abt;
-    int expectedModCount;
+    protected int expectedModCount;
 
     HeapView(ArrayBinaryTree abt) {
         this.abt = abt;
+        expectedModCount = abt.modCount;
+    }
+
+    abstract void sink(int n);
+    abstract void build(int n);
+
+    void insert(int value) {
+        checkModCount();
+
+        abt.insert(value);
         expectedModCount = abt.modCount;
     }
 
@@ -19,25 +28,108 @@ public abstract class HeapView implements AutoCloseable {
         return abt.tree[0];
     }
 
-    abstract int pop();
-
-    void insert(int value) {
+    int pop() {
         checkModCount();
-        abt.insert(value);
-        expectedModCount = abt.modCount;
+
+        int top = abt.tree[0];
+        abt.tree[0] = abt.tree[abt.size - 1];
+        abt.tree[abt.size - 1] = 0;
+        abt.size--;
+
+        if (abt.size > 1) {
+            sink(abt.size);
+        } else if (abt.size == 0) {
+            close();
+        }
+
+        expectedModCount = ++abt.modCount;
+        return top;
+    }
+
+    int replace(int value) {
+        checkModCount();
+
+        int top = abt.tree[0];
+        abt.tree[0] = value;
+
+        if (abt.size > 1) {
+            sink(abt.size);
+        }
+
+        expectedModCount = ++abt.modCount;
+        return top;
+    }
+
+    void sort() {
+        checkModCount();
+
+        for (int i = abt.size - 1; i > 0; i--) {
+            int temp = abt.tree[0];
+            abt.tree[0] = abt.tree[i];
+            abt.tree[i] = temp;
+
+            sink(i);
+        }
+        expectedModCount = ++abt.modCount;
+
+        // The heap now becomes a sorted list
+        // We close the view to prevent further operations
+        close();
+    }
+
+    public String toString() {
+        int level = 0;
+        int nodes = 1; // number of nodes in current level
+        int prints = 0; // number of nodes printed in current level
+        StringBuilder sb = new StringBuilder();
+        sb.append("Heap View (size ").append(abt.size).append("):\n");
+
+        for (int i = 0; i < abt.size; i++) {
+            if (prints == 0) {
+                sb.append("Level ").append(level).append(" [ ");
+            }
+
+            sb.append(String.format("%4d", abt.tree[i]));
+            prints++;
+
+            if (prints == nodes) {
+                sb.append("]\n");
+                prints = 0;
+                nodes <<= 1;
+                level++;
+            }
+        }
+
+        // In case the last level was not filled up with max number of nodes
+        if (prints > 0 && prints < nodes) sb.append("]\n");
+
+        return sb.toString().trim();
     }
 
     void checkModCount() {
-        if (abt.modCount != expectedModCount)
+        if (abt == null) {
+            throw new IllegalStateException("View is released and no longer valid.");
+        }
+        if (abt.modCount != expectedModCount) {
             throw new ConcurrentModificationException("Tree mutated outside Heap view");
+        }
+    }
+
+    void sync() {
+        if (abt == null) {
+            throw new IllegalStateException("View is released and no longer valid.");
+        }
+
+        build(abt.size);
+        expectedModCount = ++abt.modCount;
     }
 
     @Override
     public void close() {
-        if (abt != null) {
-            abt.clearView();
-            this.abt = null;
-        }
+        if (abt == null) return;
+
+        abt.clearView();
+        this.abt = null;
     }
 }
 
@@ -46,27 +138,12 @@ class MaxHeapView extends HeapView {
         super(abt);
     }
 
-    int pop() {
-        checkModCount();
-        if (abt.size == 0) {
-            abt.activeMaxView = null;
-            throw new NoSuchElementException();
-        }
+    void sink(int n) {
+        HeapUtil.sinkMax(abt.tree, 0, n);
+    }
 
-        int top = abt.tree[0];
-
-        abt.tree[0] = abt.tree[abt.size - 1];
-        abt.tree[abt.size - 1] = 0;
-        abt.size--;
-
-        if (abt.size > 0) {
-            HeapUtil.sinkMax(abt.tree, 0, abt.size);
-        } else {
-            abt.activeMaxView = null;
-        }
-
-        expectedModCount = ++abt.modCount;
-        return top;
+    void build(int n) {
+        HeapUtil.buildMaxHeap(abt.tree, n);
     }
 }
 
@@ -75,26 +152,11 @@ class MinHeapView extends HeapView {
         super(abt);
     }
 
-    int pop() {
-        checkModCount();
-        if (abt.size == 0) {
-            abt.activeMinView = null;
-            throw new NoSuchElementException();
-        }
+    void sink(int n) {
+        HeapUtil.sinkMin(abt.tree, 0, n);
+    }
 
-        int top = abt.tree[0];
-
-        abt.tree[0] = abt.tree[abt.size - 1];
-        abt.tree[abt.size - 1] = 0;
-        abt.size--;
-
-        if (abt.size > 0) {
-            HeapUtil.sinkMin(abt.tree, 0, abt.size);
-        } else {
-            abt.activeMinView = null;
-        }
-
-        expectedModCount = ++abt.modCount;
-        return top;
+    void build(int n) {
+        HeapUtil.buildMinHeap(abt.tree, n);
     }
 }
