b3d018bc1d1a4097d2afc67e5d74f5098de67740
Huy (Randy) <huyvlam@gmail.com>
2026-07-16 18:26:07 -0700

    Update person class to implement comparable

--- src/main/java/mymodel/Person.java
+++ src/main/java/mymodel/Person.java
@@ -1,10 +1,28 @@
 package mymodel;
 
-public class Person {
+public class Person implements Comparable<Person> {
     public String name;
     public int age;
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return Integer.compare(this.age, other.age);
+    }
+    @Override
+    public String toString() {
+        return name +  " (" + age + ")";
     }
 }
